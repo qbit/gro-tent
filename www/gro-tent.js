8aba9f1ee199eb99ed11fac523f15fd53389d9ab
Aaron Bieber <deftly@gmail.com>
2014-10-24 14:48:53 -0600

    split out graphs

--- www/gro-tent.js
+++ www/gro-tent.js
@@ -2,35 +2,77 @@ google.load('visualization', '1', {
     packages: ['corechart']
 });
 
+function makeChart(name, data, id, row_defs) {
+    var tmp_data = new google.visualization.DataTable();
+
+    var i, l;
+
+    for (i = 0, l = row_defs.length; i < l; i++) {
+	tmp_data.addColumn(row_defs[i].type, row_defs[i].name);
+    }
+
+    tmp_data.addRows(data.length || 0);
+
+    for (i = 0, l = data.length; i < l; i++) {
+	var row = data[i];
+	var j, k;
+
+	for (j = 0, k = row_defs.length; j < k; j++) {
+	    var value = row[row_defs[j].value];
+	    switch (row_defs[j].type) {
+		case "datetime":
+		    value = new Date(value);
+		    break;
+		case "number":
+		    value = parseFloat(value);
+ 		    break;
+	    }
+
+	    tmp_data.setCell(i, j, value);
+	}
+    }
+
+    var chart = new google.visualization.LineChart($('#'+id).get(0));
+
+    chart.draw(tmp_data, {
+	title: name
+    });
+}
+
 google.setOnLoadCallback(function() {
     $.ajax({
 	url: 'https://data.sparkfun.com/output/xRl4Mb5XbMcOwQA5dr2R.json',
 	data: {page: 1},
 	dataType: 'jsonp',
     }).done(function(results) {
-	var data = new google.visualization.DataTable();
-	data.addColumn('datetime', 'Time');
-	data.addColumn('number', 'Humidity');
-	data.addColumn('number', 'Temp (c)');
-	data.addColumn('number', 'Temp (f)');
-	data.addColumn('number', 'Lux');
-	data.addColumn('number', 'Dew Point');
+	makeChart('Temperature', results, 'temp', [
+	    {type: 'datetime', name: 'Time', value: 'timestamp'},
+	    {type: 'number', name: 'Temp (c)', value: 'temp_c'},
+	    {type: 'number', name: 'Temp (f)', value: 'temp_f'}
+	]);
+	makeChart('Humidity', results, 'humid', [
+	    {type: 'datetime', name: 'Time', value: 'timestamp'},
+	    {type: 'number', name: 'Humidity', value: 'humidity'}
+	]);
 
-	$.each(results, function (i, row) {
-	    data.addRow([
-		(new Date(row.timestamp)),
-		parseFloat(row.humidity),
-		parseFloat(row.temp_c),
-		parseFloat(row.temp_f),
-		parseFloat(row.lux),
-		parseFloat(row.dew_p)
-	    ]);
-	});
+	makeChart('Lux', results, 'lux', [
+	    {type: 'datetime', name: 'Time', value: 'timestamp'},
+	    {type: 'number', name: 'Lux', value: 'lux'}
+	]);
+	
+	makeChart('Temperature with Humidity', results, 'temp_h', [
+	    {type: 'datetime', name: 'Time', value: 'timestamp'},
+	    {type: 'number', name: 'Humidity', value: 'humidity'},
+	    {type: 'number', name: 'Dew point', value: 'dew_p'},
+	    {type: 'number', name: 'Temp (c)', value: 'temp_c'}
+	]);
 
-	var chart = new google.visualization.LineChart($('#chart').get(0));
-
-	chart.draw(data, {
-	    title: 'Grow Tent'
-	});
+	makeChart('All', results, 'all', [
+    	    {type: 'datetime', name: 'Time', value: 'timestamp'},
+	    {type: 'number', name: 'Humidity', value: 'humidity'},
+	    {type: 'number', name: 'Lux', value: 'lux'},
+	    {type: 'number', name: 'Dew point', value: 'dew_p'},
+	    {type: 'number', name: 'Temp (c)', value: 'temp_c'}
+   	]); 
     });
 });
