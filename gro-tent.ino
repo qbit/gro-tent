6588ec70a4d6feeb157421f1379b32ccce902600
Aaron Bieber <deftly@gmail.com>
2014-12-07 09:48:03 -0700

    abilitiy to change humidity from core

--- gro-tent.ino
+++ gro-tent.ino
@@ -1,4 +1,7 @@
 // This #include statement was automatically added by the Spark IDE.
+#include "WebServer/WebServer.h"
+
+// This #include statement was automatically added by the Spark IDE.
 #include "Adafruit_TSL2561_U/Adafruit_TSL2561_U.h"
 
 // This #include statement was automatically added by the Spark IDE.
@@ -7,9 +10,20 @@
 // This #include statement was automatically added by the Spark IDE.
 #include "Adafruit_DHT/Adafruit_DHT.h"
 
-#define DHTPIN 2            // what pin we're connected to
-#define DELAY 30000         // time to wait between measurements
-#define DHTTYPE DHT22	       // DHT 22 (AM2302)
+#define DHTPIN D2            // what pin we're connected to
+#define DELAY 60000         // time to wait between measurements
+#define DHTTYPE DHT22		// DHT 22 (AM2302)
+
+#define RELAY1 D4 // humidifier
+#define RELAY2 D5 // fan
+#define RELAY3 D6 // light
+#define RELAY4 D3 // empty
+
+#define PREFIX "/monitor"
+WebServer webserver(PREFIX, 80);
+
+int humidity = 80;
+int oldMinute = Time.minute();
 
 // Connect pin 1 (on the left) of the sensor to +5V
 // Connect pin 2 of the sensor to whatever your DHTPIN is
@@ -48,87 +62,127 @@ void displaySensorDetails(void)
   delay(500);
 }
 
+void monitorValues(WebServer &server, WebServer::ConnectionType type, char *, bool) {
+    if (type == WebServer::POST) {
+		bool repeat;
+		char name[16], value[16];
+		
+		do {
+			repeat = server.readPOSTparam(name, 16, value, 16);
+			if (strcmp(name, "humidity") == 0) {
+				humidity = strtoul(value, NULL, 10);
+			}
+			if (strcmp(name, "lights") == 0) {
+			    // Something to toggle the lights.
+			    // Need to keep state so we can toggle / update the webpage with correct state.
+			}
+		} while (repeat);
+					   
+		server.httpSeeOther(PREFIX);
+		return;
+	}
+
+	server.httpSuccess();
+
+	if (type == WebServer::GET) {
+		P(message) = "<!DOCTYPE html><html><head>"
+			"<title>Monitor</title>"
+			"<link href='http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/themes/base/jquery-ui.css' rel=stylesheet />"
+			//"<meta http-equiv='Content-Script-Type' content='text/javascript'>"
+			"<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js'></script>"
+			"<script src='http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js'></script>"
+			"<style> #slider { margin: 10px; } </style>"
+			"<script>"
+			"var lval = false;"
+			"function changeH(event, ui) { $('#indicator').text(ui.value); $.post('/monitor', { humidity: ui.value } ); } "
+			"function changeL(event, ui) { lval = !lval; $.post('/monitor', {lights: lval}); }"
+			"$(document).ready(function(){ $('#slider').slider({min: 0, max:100, change:changeH}); $('#lights').click(changeL); });";
+		P(mid) =
+			"</script>"
+			"</head>"
+			"<body style='font-size:62.5%;'>"
+			"<h1>Humidity</h1>"
+			"<div id=slider></div>"
+			"<p id=indicator>"
+			"<input type='checkbox' id='lights'><label for='lights'>Lights</label>";
+			
+		P(foot) = "</p>"
+			"</body>"
+			"</html>";
+			
+	   server.printP(message);
+	   server.print("$(document).ready(function(){ $('#slider').slider('value', " + String(humidity) + "); });");
+	   server.printP(mid);
+	   server.print("<p>Desired humidity:" + String(humidity) + "</p>");
+	   server.print("<p>Current humidity:" + String(dht.getHumidity()) + "</p>");
+	   server.printP(foot);
+	}
+}
+
 void setup() {
-     Serial.begin(9600); // requried for the http lib
-     delay(1000); // per the doc, delay before we do anything with the DHT sensor 
-     
-     if (debug) {
-         displaySensorDetails();
-	 }
-	 
+	Serial.begin(9600); // requried for the http lib
+	delay(1000); // per the doc, delay before we do anything with the DHT sensor 
+	
+	Time.zone(-7);
+	
+	if (debug) {
+	    displaySensorDetails();
+	}
+	
     if (!tsl.begin()) {
         Serial.println("Can't talk to lux sensor");
         while(1);
     } 
 
     tsl.enableAutoRange(true);
-    tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);
+
+    /* Changing the integration time gives you better sensor resolution (402ms = 16-bit data) */
+    // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
+    tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium resolution and speed   */
+    // tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
     
-    dht.begin();
+    webserver.setDefaultCommand(&monitorValues);
+    webserver.begin();
+	
+	dht.begin();
+	
+	//delay(5000);
+	
+    pinMode(RELAY1, OUTPUT);
+    pinMode(RELAY2, OUTPUT);
+    pinMode(RELAY3, OUTPUT);
+    pinMode(RELAY4, OUTPUT);
+    // Initialize all relays to an OFF state
+    digitalWrite(RELAY1, LOW);
+    digitalWrite(RELAY2, LOW);
+    digitalWrite(RELAY3, LOW);
+    digitalWrite(RELAY4, LOW);
+
 }
 
-void loop() {
-// Wait a few seconds between measurements.
-   if (!debug) {
-       delay(DELAY);
-   } else {
-       delay(5000);
-   }
-// Reading temperature or humidity takes about 250 milliseconds!
-// Sensor readings may also be up to 2 seconds 'old' (its a 
-// very slow sensor)
-   float h = dht.getHumidity();
-// Read temperature as Celsius
-   float t = dht.getTempCelcius();
-// Read temperature as Farenheit
-   float f = dht.getTempFarenheit();
-   
-   int tm = Time.now();
-   
-   float l = 0;
-   
-   sensors_event_t event;
-   
-   tsl.getEvent(&event);
-   
-   if (event.light) {
-       l = event.light;
-   }
-       
-// Check if any reads failed and exit early (to try again).
-   if (isnan(h) || isnan(t) || isnan(f)) {
-      if (debug) {
-         Serial.println("Failed to read from DHT sensor!");
-      }
-      return;
-   }
+void sendData(String reqstr) {
+    // http://data.sparkfun.com/input/[publicKey]?private_key=[privateKey]&dew_p=[value]&heat_idx_c=[value]&humidity=[value]&temp_c=[value]&temp_f=[value]&temp_k=[value]&time=[value]
 
-// Compute heat index
-// Must send in temp in Fahrenheit!
-   float hi = dht.getHeatIndex();
-   float dp = dht.getDewPoint();
-   float k = dht.getTempKelvin();
-
-// http://data.sparkfun.com/input/[publicKey]?private_key=[privateKey]&dew_p=[value]&heat_idx_c=[value]&humidity=[value]&temp_c=[value]&temp_f=[value]&temp_k=[value]&time=[value]
-
-    request.hostname = "data.sparkfun.com";
-    request.port = 80;
-    request.path = "/input/xRl4Mb5XbMcOwQA5dr2R";
+    request.hostname = "block.bold.daemon";
+    request.port = 8080;
+    request.path = "/data";
     
+    request.path += reqstr;
+    /*
     request.path += "?";
-    request.path += "private_key=omgnoyoudidnt&";
     request.path += "dew_p=" + String(dp) + "&";
     request.path += "heat_idx_c=" + String(hi) + "&";
     request.path += "humidity=" + String(h) +"&";
-    request.path += "temp_f=" + String(f) + "&";
     request.path += "temp_c=" + String(t) + "&";
-    request.path += "temp_k=" + String(k) + "&";
     request.path += "lux=" + String(l) + "&";
-    request.path += "time=" + String(tm);
+    request.path += "itime=" + String(tm);
+    */
     
     if (!debug) {
         http.get(request, response, headers);
     } else {
+        Serial.println(reqstr);
+        /*
         Serial.println(request.path);
     	Serial.print("Humid: "); 
     	Serial.print(h);
@@ -151,5 +205,96 @@ void loop() {
         Serial.print(" - time: ");
     	Serial.print(tm);
     	Serial.println("");
+    	*/
     }
+}
+
+void updateState() {
+    
+}
+
+void recordData() {
+    // Reading temperature or humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (its a 
+    // very slow sensor)
+	float h = dht.getHumidity();
+    // Read temperature as Celsius
+	float t = dht.getTempCelcius();
+    // Read temperature as Farenheit
+	float f = dht.getTempFarenheit();
+	
+	int tm = Time.now();
+	int hour = Time.hour(tm);
+	
+	float l = 0;
+	
+	if (h > humidity) {
+	    digitalWrite(RELAY1, LOW); // humidifier
+	    digitalWrite(RELAY2, LOW);
+	} else {
+	    digitalWrite(RELAY1, HIGH);
+	    digitalWrite(RELAY2, HIGH);
+	}
+	
+	if (hour >= 20 || hour <= 8) {
+	    digitalWrite(RELAY3, LOW);
+	} else {
+	    digitalWrite(RELAY3, HIGH);
+	}
+	
+	sensors_event_t event;
+	
+	tsl.getEvent(&event);
+	
+	if (event.light) {
+	    l = event.light;
+	} else {
+	    Serial.println("Failed to read from Light sensor!");
+	}
+	
+// Check if any reads failed and exit early (to try again).
+	if (isnan(h) || isnan(t) || isnan(f)) {
+		Serial.println("Failed to read from DHT sensor!");
+		return;
+	}
+
+// Compute heat index
+// Must send in temp in Fahrenheit!
+	float hi = dht.getHeatIndex();
+	float dp = dht.getDewPoint();
+	float k = dht.getTempKelvin();
+	
+	if (isnan(dp)) {
+	    dp = 0;
+	}
+	
+	String reqstr = "";
+    
+    reqstr += "?";
+    reqstr += "dew_p=" + String(dp) + "&";
+    reqstr += "heat_idx_c=" + String(hi) + "&";
+    reqstr += "humidity=" + String(h) +"&";
+    reqstr += "temp_c=" + String(t) + "&";
+    reqstr += "lux=" + String(l) + "&";
+    reqstr += "itime=" + String(tm);
+    
+    sendData(reqstr);
+}
+
+void loop() {
+    
+    char buff[64];
+    int len = 64;
+
+    /* process incoming connections one at a time forever */
+    webserver.processConnection(buff, &len);
+    
+    int newMinute = Time.minute();
+    
+    if (oldMinute < newMinute) {
+        recordData();
+        oldMinute = newMinute;
+    }
+    
+    //delay(10);
 }
